c302a3c50c3523a9caae8fc67a146400c0334db4
Jarod Sneed <jarodsneed@Jarods-MacBook.local>
2019-05-31 19:22:43 -0400

    part 2

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,15 +1,15 @@
 import 'phaser';
- 
+
 export default class BootScene extends Phaser.Scene {
   constructor () {
     super('Boot');
   }
- 
+
   preload () {
-    this.load.image('zlogo', '../assets/zenva_logo.png');
+    this.load.image('logo', 'assets/zenva_logo.png');
   }
- 
+
   create () {
-    this.scene.start('Preloader')
+    this.scene.start('Preloader');
   }
 };
